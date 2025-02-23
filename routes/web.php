a87b32a0374b6c06c79f902f0e58e8757448dc91
zeesee123 <zubinchadha@gmail.com>
2025-02-24 00:10:32 +0530

    clean

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,7 @@ Route::get('/home',function(){
     return view('pages.homepage');
 });
 
-// Route::get('/choot',function(){
-//     return view('pages.homepage');
-// });
+
 
 Route::post('/add_homepage',[HomepageController::class,'add_homepage']);
 
