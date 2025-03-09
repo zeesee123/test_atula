90bdb54b3997be6207fbda54375ef06c46531566
zeesee123 <zubinchadha@gmail.com>
2025-03-09 23:37:39 +0530

    new post

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\AboutpageController;
 use App\Http\Controllers\AgriventureController;
+use App\Http\Controllers\BusinesspageController;
+use App\Http\Controllers\AgriventurepageController;
 use App\Http\Controllers\AgroforestrypageController;
 use App\Http\Controllers\EcoinitiativepageController;
-use App\Http\Controllers\BusinesspageController;
 
 
 /*
@@ -78,6 +79,17 @@ Route::group(['middleware'=>['auth']],function(){
 
     //routes for aboutpage
     Route::post('/add_aboutpage',[AboutpageController::class,'add_aboutpage']);
+
+    Route::get('/abouttable/{section}',[HomepageController::class,'loadtable']);
+
+    Route::get('/get_resource_about/{type}/{id}',[HomepageController::class,'get_resource']);
+
+    Route::post('/update_aboutsection/{sectionType}',[HomepageController::class,'update_resource']);
+
+    Route::post('/remove_aboutsection/{sectionType}',[HomepageController::class,'delete_resource']);
+
+    //routes for AnMagriventure
+    Route::post('/add_agriventurepage',[AgriventurepageController::class,'add_aboutpage']);
 
     Route::get('/abouttable/{section}',[HomepageController::class,'loadtable']);
 
