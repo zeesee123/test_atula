0aeebe1d6759c41bb02480ed7df5fbda259aa86b
zeesee123 <zubinchadha@gmail.com>
2025-03-16 18:21:41 +0530

    new changes

--- app/Http/Controllers/EcoinitiativepageController.php
+++ app/Http/Controllers/EcoinitiativepageController.php
@@ -13,23 +13,23 @@ class EcoinitiativepageController extends Controller
 
             DB::beginTransaction();
 
-            $model = Agroforestrypage::first() ?? new Agroforestry();
+            $model = Ecoinitiativepage::first() ?? new Ecoinitiativepage();
 
 
             $model->sec1title = $r->sec1title;
-            $model->sec1text = $r->sec1text;
+            
             $model->sec1btn_text = $r->sec1btn_text;
             $model->sec1btn_url = $r->sec1btn_url;
 
       
             if ($r->hasFile('sec1image')) {
-                if (!empty($model->sec1image) && File::exists(public_path('agroforestrypage/'.$model->sec1image))) {
-                    File::delete(public_path('agroforestrypage/'.$model->sec1image));
+                if (!empty($model->sec1image) && File::exists(public_path('ecoinitiativepage/'.$model->sec1image))) {
+                    File::delete(public_path('ecoinitiative/'.$model->sec1image));
                 }
                 $video = $r->file('sec1image');
                 // $videoName = time() . '_' . $video->getClientOriginalName();
                 $videoName = $video->hashName();
-                $videoPath = 'agroforestrypage/'; // Set the upload directory
+                $videoPath = 'ecoinitiative/'; // Set the upload directory
                 $video->move(public_path($videoPath), $videoName);
                 $model->sec1image = $videoName; // Save path in DB
                 
@@ -38,23 +38,47 @@ class EcoinitiativepageController extends Controller
             
 
             $model->sec2title = $r->sec2title;
-            $model->sec2addtext = $r->sec2addtext;
+            
             $model->sec2text = $r->sec2text;
             
       
       try{
         if ($r->hasFile('sec2image')) {
-            if (!empty($model->sec2image) && File::exists(public_path('agroforestrypage/'.$model->sec2image))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec2image));
+            if (!empty($model->sec2image) && File::exists(public_path('ecoinitiativepage/'.$model->sec2image))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec2image));
             }
             $gif = $r->file('sec2image');
             // $videoName = time() . '_' . $video->getClientOriginalName();
             $gifName = $gif->hashName();
-            $gifPath = 'agroforestrypage/'; // Set the upload directory
+            $gifPath = 'ecoinitiativepage/'; // Set the upload directory
             $gif->move(public_path($gifPath), $gifName);
             $model->sec2image = $gifName; // Save path in DB
             
         }
+
+        
+
+        
+
+        
+        $sec2_stitle = $r->input('sec2titlel'); // Ensure this is fetched too
+        
+        $sec2_stext = $r->input('sec2textl'); // Ensure this is fetched too
+
+        if ($sec2_stitle) {
+            foreach ($sec2_stitle as $key => $value) {
+                if ($image) {
+                    
+
+                    $section2 = new EcoinitiativepageSection2();
+                    
+                    $section2->sec2titlel = $value ?? null;
+                    $section2->sec2textl = $sec2_stext[$key] ?? null;
+                    $section2->save();
+                }
+            }
+        }
+       
      
 
         $model->sec3title = $r->sec3title;
@@ -68,9 +92,9 @@ class EcoinitiativepageController extends Controller
             foreach ($sec3_slogo as $key => $image) {
                 if ($image) {
                     $namez = $image->hashName();
-                    $image->move(public_path('agroforestrypage/'), $namez);
+                    $image->move(public_path('ecoinitiativepage/'), $namez);
 
-                    $section3 = new AgroforestrypageSection3();
+                    $section3 = new EcoinitiativepageSection3();
                     $section3->sec3imagel = $namez;
                     
                     $section3->sec3textl = $sec3_stext[$key] ?? null;
@@ -86,13 +110,13 @@ class EcoinitiativepageController extends Controller
         $model->sec4quote=$r->sec4quote;
 
         if ($r->hasFile('sec4image')) {
-            if (!empty($model->sec4image) && File::exists(public_path('agroforestrypage/'.$model->sec4image))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec4image));
+            if (!empty($model->sec4image) && File::exists(public_path('ecoinitiativepage/'.$model->sec4image))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec4image));
             }
             $video = $r->file('sec4image');
             // $videoName = time() . '_' . $video->getClientOriginalName();
             $videoName = $video->hashName();
-            $videoPath = 'agroforestrypage/'; // Set the upload directory
+            $videoPath = 'ecoinitiativepage/'; // Set the upload directory
             $video->move(public_path($videoPath), $videoName);
             $model->sec1image = $videoName; // Save path in DB
             
@@ -118,11 +142,11 @@ class EcoinitiativepageController extends Controller
              foreach ($sec5_slogo1 as $key => $image) {
                  if ($image) {
                      $namez = $image->hashName();
-                     $image->move(public_path('agroforestrypage/'), $namez);
+                     $image->move(public_path('ecoinitiativepage/'), $namez);
 
                      
  
-                     $section5 = new AgroforestrypageSection5();
+                     $section5 = new EcoinitiativepageSection5();
                      $section5->sec5imagel = $namez;
          
          
@@ -145,8 +169,8 @@ class EcoinitiativepageController extends Controller
         
 
         if ($r->hasFile('sec6image')) {
-            if (!empty($model->sec6image) && File::exists(public_path('agroforestrypage/'.$model->sec6image))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec6image));
+            if (!empty($model->sec6image) && File::exists(public_path('ecoinitiativepage/'.$model->sec6image))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec6image));
             }
             $video = $r->file('sec6image');
             // $videoName = timerepage/'; // Set the upload directory
@@ -169,11 +193,11 @@ class EcoinitiativepageController extends Controller
              foreach ($sec7_slogo as $key => $image) {
                  if ($image) {
                      $namez = $image->hashName();
-                     $image->move(public_path('agroforestrypage/'), $namez);
+                     $image->move(public_path('ecoinitiativepage/'), $namez);
 
                      
  
-                     $section7 = new AgroforestrypageSection7();
+                     $section7 = new EcoinitiativepageSection7();
                      $section7->sec7imagel = $namez;
          
          
@@ -207,7 +231,7 @@ class EcoinitiativepageController extends Controller
                     $section8 = new Testimonial();
                     $section8->image= $namez;
                     $section8->name= $sec8_sname[$key] ?? null;
-                    $section->page='agroforestry';
+                    $section->page='ecoinitiative';
                     $section8->text = $sec8_stext[$key] ?? null;
                     $section8->save();
                 }
@@ -227,8 +251,8 @@ class EcoinitiativepageController extends Controller
         $model->sec10btn_url = $r->sec10btn_url;
 
         if ($r->hasFile('sec10_image1')) {
-            if (!empty($model->sec10_image1) && File::exists(public_path('agroforestrypage/'.$model->sec10_image1))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec10_image1));
+            if (!empty($model->sec10_image1) && File::exists(public_path('ecoinitiativepage/'.$model->sec10_image1))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec10_image1));
             }
             $video = $r->file('sec10_image1');
             // $videoName = timerepage/'; // Set the upload directory
@@ -238,8 +262,8 @@ class EcoinitiativepageController extends Controller
         }
 
         if ($r->hasFile('sec10_image2')) {
-            if (!empty($model->sec10_image2) && File::exists(public_path('agroforestrypage/'.$model->sec10_image2))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec10_image2));
+            if (!empty($model->sec10_image2) && File::exists(public_path('ecoinitiativepage/'.$model->sec10_image2))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec10_image2));
             }
             $video = $r->file('sec10_image2');
             // $videoName = timerepage/'; // Set the upload directory
@@ -262,11 +286,11 @@ class EcoinitiativepageController extends Controller
             foreach ($sec11_slogo1 as $key => $image) {
                 if ($image) {
                     $namez = $image->hashName();
-                    $image->move(public_path('agroforestrypage/'), $namez);
+                    $image->move(public_path('ecoinitiativepage/'), $namez);
 
                     
 
-                    $section11 = new AgroforestrypageSection11();
+                    $section11 = new EcoinitiativepageSection11();
                     $section11->sec11imagel = $namez;
         
         
@@ -296,11 +320,11 @@ class EcoinitiativepageController extends Controller
             foreach ($sec12_slogo1 as $key => $image) {
                 if ($image) {
                     $namez = $image->hashName();
-                    $image->move(public_path('agroforestrypage/'), $namez);
+                    $image->move(public_path('ecoinitiativepage/'), $namez);
 
                     
 
-                    $section12 = new AgroforestrypageSection12();
+                    $section12 = new EcoinitiativepageSection12();
                     $section12->sec12imagel = $namez;
         
         
@@ -332,7 +356,7 @@ class EcoinitiativepageController extends Controller
 
                     
 
-                    $section13 = new AgroforestrypageSection13();
+                    $section13 = new EcoinitiativepageSection13();
                     
         
                     $section12->sec13textl = $sec13_stext[$key] ?? null;
@@ -345,8 +369,8 @@ class EcoinitiativepageController extends Controller
         //section 14
 
         if ($r->hasFile('sec14image')) {
-            if (!empty($model->sec14image) && File::exists(public_path('agroforestrypage/'.$model->sec14image))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec14image));
+            if (!empty($model->sec14image) && File::exists(public_path('ecoinitiativepage/'.$model->sec14image))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec14image));
             }
             $video = $r->file('sec14image');
             // $videoName = timerepage/'; // Set the upload directory
@@ -362,8 +386,8 @@ class EcoinitiativepageController extends Controller
         $model->sec15btn_url = $r->sec15btn_url;
 
         if ($r->hasFile('sec15image')) {
-            if (!empty($model->sec15image) && File::exists(public_path('agroforestrypage/'.$model->sec15image))) {
-                File::delete(public_path('agroforestrypage/'.$model->sec15image));
+            if (!empty($model->sec15image) && File::exists(public_path('ecoinitiativepage/'.$model->sec15image))) {
+                File::delete(public_path('ecoinitiativepage/'.$model->sec15image));
             }
             $video = $r->file('sec15image');
             // $videoName = timerepage/'; // Set the upload directory
