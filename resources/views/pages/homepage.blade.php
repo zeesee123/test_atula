1a26789e61d0b6d2a14e86c4c0c04fa0ec281469
zeesee123 <zubinchadha@gmail.com>
2025-05-05 10:44:37 +0530

    fix for null

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -141,13 +141,13 @@ https://cdn.jsdelivr.net/npm/filepond-plugin-image-preview@4.6.12/dist/filepond-
        src="{{ asset_env('images/default.jpg') }}" 
        width="400" 
        alt="Default Video Thumbnail"
-       style="{{ $section->sec1_vid ? 'display:none;' : '' }}">
+       style="{{ $section?->sec1_vid ? 'display:none;' : '' }}">
   
   <video class="videoPreview" 
          width="400" 
          controls 
          style="{{ $section->sec1_vid ? 'display:block;' : 'display:none;' }}">
-      <source src="{{ $section?->sec1_vid ? asset_env('homepage/'.$section->sec1_vid) : '' }}" type="video/mp4">
+      <source src="{{ $section->sec1_vid ? asset_env('homepage/'.$section->sec1_vid) : '' }}" type="video/mp4">
   </video>
           </div>
         </div>
@@ -187,7 +187,7 @@ https://cdn.jsdelivr.net/npm/filepond-plugin-image-preview@4.6.12/dist/filepond-
 <video class="videoPreview" 
        width="400" 
        controls 
-       style="{{ $section->sec2gif ? 'display:block;' : 'display:none;' }}">
+       style="{{ $section?->sec2gif ? 'display:block;' : 'display:none;' }}">
     <source src="{{ $section->sec2gif ? asset_env('homepage/'.$section->sec2gif) : '' }}" type="video/mp4">
 </video>
         </div>
